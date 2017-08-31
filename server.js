7e965f8b2c2ec5518c3c36963314d8ca635531f4
Miguel Gomez <miguelcode59@gmail.com>
2017-08-31 00:26:05 -0400

    establish mongodb connection

--- server.js
+++ server.js
@@ -8,29 +8,30 @@ var server = app.listen(4000);
 //Serving static files in Express
 app.use(express.static('client'));
 
-//set up socket on server side
-var io = socket(server);
-io.on('connection', (socket) => {
-	//console.log('made socket connection');
-	
-	socket.on('chat', (data) => {
-		//console.log(data);
-		io.sockets.emit('chat', data); //send data to messageBoard tag to all sockets connected to 	
-	});
-
-	socket.on('typing', (data) => {
-		socket.broadcast.emit('typing', data);
-		
-	});
-	
-		 
-});
+var io = socket(server); //set up socket on server side
 
 MongoClient.connect('mongodb://127.0.0.1/test', (err, db) => {
-	if (err) {
-		throw err;
-	} 	
-	console.log('it is working');
+	//if (err) throw err; 
+
+	io.on('connection', (socket) => {
+		//console.log('made socket connection');
+	
+		socket.on('chat', (data) => {
+			//console.log(data);
+			io.sockets.emit('chat', data); //send data to messageBoard tag to all sockets connected to 	
+		});
+
+		socket.on('typing', (data) => {
+			socket.broadcast.emit('typing', data);
+		
+		});	 
+	});
+
+
+
+
+
+
 });
 
 
